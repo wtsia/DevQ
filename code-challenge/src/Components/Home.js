1bb3218167a08bca7446514e5221ef6521b1881f
Gabriel Sharpton <3bag@Tricias-MBP.lan1>
2019-10-21 10:58:06 -0500

    filling in home page

--- code-challenge/src/Components/Home.js
+++ code-challenge/src/Components/Home.js
@@ -1,9 +1,22 @@
 import React, { Component } from "react";
 
 class Home extends Component {
-  render() {
-    return <h1>Home Test</h1>;
-  }
+    // constructor(props) {
+    //     super(props)
+    // }
+    render() {
+        return(
+            <div className="questions">
+            <h1>Hello world!</h1>
+            <button>HTML</button>
+            <button>JavaScript</button>
+            <button>Express</button>
+            <button>CSS</button>
+            <button>React</button>
+            <button>Node.js</button>
+            </div>
+        )
+    }
 }
 
 export default Home;
