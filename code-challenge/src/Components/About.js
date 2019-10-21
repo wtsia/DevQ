a04dd1381fe1556182b5e04fed1f721cf50fe718
Cooper Tuch <coopertuch@gmail.com>
2019-10-21 10:15:24 -0700

    placement

--- code-challenge/src/Components/About.js
+++ code-challenge/src/Components/About.js
@@ -1,17 +1,30 @@
 import React, { Component } from "react";
-import "./About.css"
-
+import "./About.css";
 
 class About extends Component {
   render() {
     return (
+      <div className="container">
         <div className="About">
-    <h1>About our Project!</h1>
-        <p>Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.</p>
-        <p>Hodor. Hodor hodor, hodor. Hodor hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor; hodor hodor hodor. Hodor. Hodor hodor; hodor hodor - hodor, hodor, hodor hodor. Hodor, hodor. Hodor. Hodor, hodor hodor hodor; hodor hodor; hodor hodor hodor! Hodor hodor HODOR! Hodor hodor... Hodor hodor hodor...</p>
+          <h1>About our Project!</h1>
+          <p>
+            Well, the way they make shows is, they make one show. That show's
+            called a pilot. Then they show that show to the people who make
+            shows, and on the strength of that one show they decide if they're
+            going to make more shows. Some pilots get picked and become
+            television programs. Some don't, become nothing. She starred in one
+            of the ones that became nothing.
+          </p>
+          <p>
+            Hodor. Hodor hodor, hodor. Hodor hodor hodor hodor hodor. Hodor.
+            Hodor! Hodor hodor, hodor; hodor hodor hodor. Hodor. Hodor hodor;
+            hodor hodor - hodor, hodor, hodor hodor. Hodor, hodor. Hodor. Hodor,
+            hodor hodor hodor; hodor hodor; hodor hodor hodor! Hodor hodor
+            HODOR! Hodor hodor... Hodor hodor hodor...
+          </p>
         </div>
-        )
-
+      </div>
+    );
   }
 }
 
--- code-challenge/src/Components/Home.js
+++ code-challenge/src/Components/Home.js
@@ -1,22 +1,24 @@
 import React, { Component } from "react";
-import "./Home.css"
+import "./Home.css";
 
 class Home extends Component {
-    // constructor(props) {
-    //     super(props)
-    // }
-    render() {
-        return(
-            <div className="languages">
-            <button>HTML</button>
-            <button>JavaScript</button>
-            <button>Express</button>
-            <button>CSS</button>
-            <button>React</button>
-            <button>Node.js</button>
-            </div>
-        )
-    }
+  // constructor(props) {
+  //     super(props)
+  // }
+  render() {
+    return (
+      <div className="container">
+        <div className="languages">
+          <button>HTML</button>
+          <button>JavaScript</button>
+          <button>Express</button>
+          <button>CSS</button>
+          <button>React</button>
+          <button>Node.js</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
