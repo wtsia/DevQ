a8fadccfa879050eff70abf479e27ddd81b16298
Gabriel Sharpton <3bag@Tricias-MBP.lan1>
2019-10-22 10:32:18 -0500

    showing json data for questions

--- code-challenge/src/Components/Question.js
+++ code-challenge/src/Components/Question.js
@@ -1,24 +1,23 @@
 import React, { Component } from "react";
+import question from './Question.json'
 
 class Question extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      question: "",
-      hint: "",
-      answer: "",
-      url: "",
-      type: "",
-      id: 0
+      question: {question}
+      
     };
   }
   render() {
-    let item = this.props.questions.find(
+    let questions = this.props.questions.find(
       challenge => challenge.type === this.props.match.params.type
     );
     return (
-      <div className="container" key={item.type}>
-        <h1>{item.type}</h1>;
+      <div className="container" key={question.type}>
+        <h1>{question[0].question}</h1>
+        <h2>{question[0].type}</h2>
+        <h2>{question[0].hint}</h2>
       </div>
     );
   }
