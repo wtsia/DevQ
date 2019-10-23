edf7f22cca6c150b2a07bf22a9129400f4e42ab4
Cooper Tuch <coopertuch@gmail.com>
2019-10-23 13:31:21 -0700

    values in inputs

--- code-challenge/src/Components/Edit.js
+++ code-challenge/src/Components/Edit.js
@@ -14,16 +14,19 @@ class Edit extends Component {
 
   componentDidMount() {
     let language = this.props.match.params.id;
-    console.log(this.props)
+    console.log(this.props);
     fetch(`https://immense-citadel-86220.herokuapp.com/edit/${language}`)
       .then(res => res.json())
       .then(
         result => {
-          this.setState({
-            isLoaded: true,
-            items: result,
-            x: 0
-          },(newState) => console.log(this.state, newState));
+          this.setState(
+            {
+              isLoaded: true,
+              items: result,
+              x: 0
+            },
+            newState => console.log(this.state, newState)
+          );
         },
         error => {
           this.setState({
@@ -34,8 +37,6 @@ class Edit extends Component {
       );
   }
 
-
-
   render() {
     const { error, isLoaded, items } = this.state;
     if (error && items) {
@@ -45,48 +46,71 @@ class Edit extends Component {
     } else {
       // let current = this.state.x + 1;
       let item = items;
-      console.log(item)
-      console.log(items)
-    return (
-      <div className="container">
-        <div className="editContainer">
-          <h1>Edit</h1>
-          <div className="edit">
-            <form action={`https://immense-citadel-86220.herokuapp.com/edit/${this.props.match.params.id}?_method=put`} method="POST">
-              <p>
-                <h2>Question:</h2>
-                <input className="editPage" type="text" name="question" placeholder={items.question}/>
-              </p>
-              <p>
-                <h2>Hint: </h2>
-                <input className="editPage" type="text" name="hint" />
-              </p>
-              <p>
-                <h2>Answer: </h2>
-                <input className="editPage" type="text" name="answer" />
-              </p>
-              <p>
-                <h2>URL: </h2>
-                <input className="editPage" type="text" name="url" />
-              </p>
-              <p>
-                <h2>Type: </h2>
-                <select>
-                  <option value="HTML">HTML</option>
-                  <option value="CSS">CSS</option>
-                  <option value="JavaScript">JavaScript</option>
-                  <option value="React">React</option>
-                  <option value="Express">Express</option>
-                  <option value="Node">Node</option>
-                </select>
-              </p>
-              <input type="submit" value="Post" />
-            </form>
+      console.log(item);
+      console.log(items);
+      return (
+        <div className="container">
+          <div className="editContainer">
+            <h1>Edit</h1>
+            <div className="edit">
+              <form
+                action={`https://immense-citadel-86220.herokuapp.com/edit/${this.props.match.params.id}?_method=put`}
+                method="POST"
+              >
+                <p>
+                  <h2>Question:</h2>
+                  <input
+                    className="editPage"
+                    type="text"
+                    name="question"
+                    value={items.question}
+                  />
+                </p>
+                <p>
+                  <h2>Hint: </h2>
+                  <input
+                    className="editPage"
+                    type="text"
+                    name="hint"
+                    value={items.hint}
+                  />
+                </p>
+                <p>
+                  <h2>Answer: </h2>
+                  <input
+                    className="editPage"
+                    type="text"
+                    name="answer"
+                    value={items.answer}
+                  />
+                </p>
+                <p>
+                  <h2>URL: </h2>
+                  <input
+                    className="editPage"
+                    type="text"
+                    name="url"
+                    value={items.url}
+                  />
+                </p>
+                <p>
+                  <h2>Type: </h2>
+                  <select>
+                    <option value="HTML">HTML</option>
+                    <option value="CSS">CSS</option>
+                    <option value="JavaScript">JavaScript</option>
+                    <option value="React">React</option>
+                    <option value="Express">Express</option>
+                    <option value="Node">Node</option>
+                  </select>
+                </p>
+                <input type="submit" value="Post" />
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
-}
 }
 export default Edit;
