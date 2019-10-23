d9e490021cf75f08af75f1dcbfc0fd751bcc2e31
Cooper Tuch <coopertuch@gmail.com>
2019-10-23 09:58:32 -0700

    edit js, styling

--- code-challenge/src/Components/Edit.js
+++ code-challenge/src/Components/Edit.js
@@ -5,38 +5,38 @@ class Edit extends Component {
   render() {
     return (
       <div className="container">
-        <div className="edit">
+        <div className="editContainer">
           <h1>Edit</h1>
-          <div className="edit"> 
-          <form action="/edit/:id" method="post">
-                <p>
-                <label>Question:</label>
-                <input type="text" name="question"/>
-                </p>
-                <p>
-                <label>Hint: </label>
-                <input type="text" name="hint"/>
-                </p>
-                <p>
-                <label>Answer: </label>
-                <input type="text" name="answer"/>
-                </p>
-                <p>
-                <label>URL: </label>
-                <input type="text" name="url"/>
-                </p>
-                <p>
-                <label>Type: </label> 
-                    <select>
-                        <option value="HTML">HTML</option>
-                        <option value="CSS">CSS</option>
-                        <option value="JavaScript">JavaScript</option>
-                        <option value="React">React</option>
-                        <option value="Express">Express</option>
-                        <option value="Node">Node</option>
-                    </select>
-                </p>
-                <input type="submit" value="Post" />
+          <div className="edit">
+            <form action="/edit/:id" method="post">
+              <p>
+                <h2>Question:</h2>
+                <input className="editPage" type="text" name="question" />
+              </p>
+              <p>
+                <h2>Hint: </h2>
+                <input className="editPage" type="text" name="hint" />
+              </p>
+              <p>
+                <h2>Answer: </h2>
+                <input className="editPage" type="text" name="answer" />
+              </p>
+              <p>
+                <h2>URL: </h2>
+                <input className="editPage" type="text" name="url" />
+              </p>
+              <p>
+                <h2>Type: </h2>
+                <select>
+                  <option value="HTML">HTML</option>
+                  <option value="CSS">CSS</option>
+                  <option value="JavaScript">JavaScript</option>
+                  <option value="React">React</option>
+                  <option value="Express">Express</option>
+                  <option value="Node">Node</option>
+                </select>
+              </p>
+              <input type="submit" value="Post" />
             </form>
           </div>
         </div>
