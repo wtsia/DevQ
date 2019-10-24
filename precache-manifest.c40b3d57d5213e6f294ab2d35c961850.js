accebb32ebb5c9e5312aa2bb18822255f5b88e9d
Winston Tsia <winstontsia@gmail.com>
2019-10-24 10:50:40 -0700

    Updates

---
+++ precache-manifest.c40b3d57d5213e6f294ab2d35c961850.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1792bfc216b1d250efb37b5adfe0c5e",
+    "url": "/DevQ/index.html"
+  },
+  {
+    "revision": "c339a372064084640f1b",
+    "url": "/DevQ/static/css/main.d079c18e.chunk.css"
+  },
+  {
+    "revision": "e23bebbf2b1c6f37d0ec",
+    "url": "/DevQ/static/js/2.de518fd9.chunk.js"
+  },
+  {
+    "revision": "c339a372064084640f1b",
+    "url": "/DevQ/static/js/main.29e0f92c.chunk.js"
+  },
+  {
+    "revision": "9f33f071579091a5f98c",
+    "url": "/DevQ/static/js/runtime-main.87130514.js"
+  }
+]);
