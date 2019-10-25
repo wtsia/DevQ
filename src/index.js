4f1dbc23d0a0f96ae2cd4a0eeaa653793b1ffb71
Winston Tsia <winstontsia@gmail.com>
2019-10-25 08:10:15 -0700

    Added CSS fonts

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import './fonts/Raleway/Raleway-Regular.ttf'
 
 ReactDOM.render(
   <Router basename="/DevQ">
