ea133f9c8a8dce97fe8cd646fe80bf9d1a6b9487
Corey Hammond <coreyhammond@Coreys-Air.hitronhub.home>
2019-10-24 22:03:36 -0700

    added basic tests for <About />

--- src/Components/About.test.js
+++ src/Components/About.test.js
@@ -1,9 +1,18 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import { shallow } from "enzyme";
 
 import About from "./About";
 
 describe("About component", () => {
+  
+  //renders without crashing
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<About />, div);
+})
+
+  // shallow(<About />) before each test
   let component;
   beforeEach(() => {
     component = shallow(<About />);
@@ -11,7 +20,7 @@ describe("About component", () => {
 
   // component tests below
 
-  it("should have a header that says 'About our Project!'", () => {
-    expect(component.contains(<h1>About our Project!</h1>)).toBe(true);
+  it("should have a header that says 'About Us // Read Me'", () => {
+    expect(component.contains(<h1>About Us // Read Me</h1>)).toBe(true);
   });
 });
---
+++ src/Components/Question.test.js
@@ -0,0 +1,16 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { shallow } from "enzyme";
+// import renderer from "react-test-renderer";
+
+// import Question from "./Question";
+
+// describe("Question component", () => {
+  
+//     //renders without crashing
+//     it("renders without crashing", () => {
+//       const div = document.createElement("div");
+//       ReactDOM.render(<Question />, div);
+//   })
+
+// })
